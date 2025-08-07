042922aba53df3ce8dd29e96e6baf4c68e65d742
Ashis28 <dashashiskumar28@example.com>
2025-08-07 19:09:37 +0530

    ProductList structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
-import { Text, View } from "react-native";
-
+import { FlatList, Text, View } from "react-native";
+import products from "../assets/products.json";
+import ProductListItem from "../components/ProductListItem";
 
 export default function HomeScreen(){
     return(
-        <View>
-            <Text style={{fontSize:30}}>HomeScreen</Text>
-        </View>
+        
+            <FlatList 
+            data={products}
+            renderItem={({item})=>  <ProductListItem product={item}/> }/>
+           
     );
 }
